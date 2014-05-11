993a790f80b0d4b96b2f8f6a95a31ae3c1be5143
Matt Baker <dev@mikesierra.net>
2014-05-11 23:19:06 +0100

    added some comments

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,8 @@
  */
 $(document).ready(function() { //run scripts after page has loaded
     
-     
+//here
+    
 });
 
 /*
@@ -14,19 +15,19 @@ $(document).ready(function() { //run scripts after page has loaded
  */
 
 /*
- * Submits the adduserform to adduser action 
+ * Submits the adduserform HTML form to ajaxadduser model action 
  */
 function submitUserForm(baseurl) {
-    var status = validateForm();
-    if(status){
+    var status = validateForm(); //JS form validation
+    if(status){//if validation passed call action via AJAX
         $.ajax({
             type:'POST',
             url: base_url + '/home/ajaxadduser/',
             data:$('#adduserform').serialize(), 
             success: function(response) {
-                tableMsg(response);
-                $('#userTable').load(base_url + '/home/updateusertable');
-                $( '#adduserform' ).each(function(){
+                tableMsg(response); //show status message
+                $('#userTable').load(base_url + '/home/updateusertable'); //redraw table
+                $( '#adduserform' ).each(function(){// reset the form
                     this.reset();
                 });
             },
@@ -34,56 +35,65 @@ function submitUserForm(baseurl) {
                 alert('Form not submitted - Ajax Error: ' + exception);
             }
         });
-    } else {
-        tableMsg('{"status":"0","msg":"Please Complete All Fields"}')
+    } else {// validation isn't passed
+        tableMsg('{"status":"0","msg":"Please Complete All Fields"}')  //show status message
     }
     return false;
 };
 
+/*
+ * displays a status message below user table
+ */
 function tableMsg(data) {
-    var details = jQuery.parseJSON(data);
+    var details = jQuery.parseJSON(data); //turn JSON string into object
     
-    $('#statusMsg').hide();
+    $('#statusMsg').hide(); 
     switch (Number(details.status)) {
-        case 0:
-            $('#statusMsg').css({"background-color":"#ff6666", "outline":"1px solid #ff0000"});
+        case 0://action failed
+            $('#statusMsg').css({"background-color":"#ff6666", "outline":"1px solid #ff0000"}); //red box
             break;
-        case 1:
-             $('#statusMsg').css({"background-color":"#66ff66", "outline":"1px solid #19ff19"});
+        case 1: //action succeeded
+             $('#statusMsg').css({"background-color":"#66ff66", "outline":"1px solid #19ff19"}); //green box
             break;
     }
     $('#statusMsg').html(details.msg);
-    $('#statusMsg').show().delay(5000).fadeOut(2000);
+    $('#statusMsg').show().delay(5000).fadeOut(2000); //show message and then fade
 }
 
+/*
+ * performs simple validation of form
+ */
 function validateForm() {
     var status = true;
 
     if ($('#firstname').val() == "" || $('#firstname').val() == null) {
-        $('#fnerror').html('*Required');
+        $('#fnerror').html('*Required'); //show error on form
         status = false;
     } else {
-      $('#fnerror').html('');
+      $('#fnerror').html(''); //remove error
        status = true;
     }
     if ($('#surname').val() == "" || $('#surname').val() == null) {
-        $('#snerror').html('*Required');
+        $('#snerror').html('*Required'); //show error on form
         status = false;
     } else {
-       $('#snerror').html('');
+       $('#snerror').html('');  //remove error
         status = true;
     }
     
     return status;
 }
 
+/*
+ * deletes a user record via AJAX using ajaxdeleteuser model action
+ */
 function ajaxdeleteuser(id) {
     $.ajax({
             type:'POST',
             url: base_url + '/home/ajaxdeleteuser/' + id, 
             success: function(response) {
-                tableMsg(response);
-                $('#userTable').load(base_url + '/home/updateusertable');
+                tableMsg(response); //show status message
+                $('#userTable').load(base_url + '/home/updateusertable'); //redraw table
             },
             error: function(jqXHR, exception) {
                 alert('query not submitted - Ajax Error: ' + exception);
